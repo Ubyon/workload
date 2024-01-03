673c9da552ddd72d2e304b7fdcfc64e37819cf8f
Saleh Noordeen <saleh.noordeen@ubyon.com>
2024-01-03 15:47:14 +0530

    updating the shell scripts for Database, Storage and SaaS

---
+++ attestor/access/examples/database/rds_cleanup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pod_name=$(kubectl get pods -n internal2cdemo -o=jsonpath='{.items[0].metadata.name}' -l job-name=rdshello)
+
+# Delete the job
+kubectl delete -f /home/ubuntu/tln/internal2cdemo/podspecs/srcapp/rds-hello-job.yaml
+
+# Check if the job is deleted
+printf "Checking if the job rds-hello is deleted...\n"
+while kubectl get job rdshello -n internal2cdemo > /dev/null 2>&1; do
+  sleep 5
+done
+
+# Print the remaining pods
+printf "Remaining pods in namespace internal2cdemo:\n"
+printf "___________________________________________\n"
+kubectl get pods -n internal2cdemo | grep rdshello
+
+# Ask the attestor to propagate delete to the Control Plane
+att_pod_name=$(kubectl get pods -n internal2cdemo -l app=ubyon-workload-attestor -o=jsonpath='{.items[0].metadata.name}')
+kubectl exec -n internal2cdemo $att_pod_name -- /usr/bin/curl -v -XDELETE "http://ubyon-workload-attestor.internal2cdemo.svc.cluster.local:34323/workload-agent/v1/workloads?type=K8S-POD&name=internal2cdemo-$pod_name&namespace=internal2cdemo"
+printf "\n\n\n"
---
+++ attestor/access/examples/storage/s3_rd_access.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Step 1: Apply the YAML file to create the job
+printf "____________________________________________________________________________________\n"
+printf "## Deploy the workload -->  kubectl apply -f s3hello-job.yaml\n"
+kubectl apply -f /home/ubuntu/tln/internal2cdemo/podspecs/srcapp/s3hello-job.yaml
+
+# Wait for the job to complete
+printf "____________________________________________________________________________________\n"
+printf "## Waiting for the job to fetch the credentials...\n"
+#kubectl wait --for=condition=complete --timeout=60s job/s3hello -n internal2cdemo
+wo=$(kubectl wait --for=condition=complete --timeout=35s job/s3hello -n internal2cdemo 2>&1)
+printf "## %s...\n" "$wo"
+if [[ $wo == *"error"* ]]; then
+    echo "Unable to get credentials, exiting"
+    exit -1
+fi
+
+# Step 2: Print the pods
+printf "____________________________________________________________________________________\n"
+printf "## Get all the pods...\n"
+kubectl get pods -n internal2cdemo
+
+# Step 3: Get the pod name
+pod_name=$(kubectl get pods -n internal2cdemo -o=jsonpath='{.items[0].metadata.name}' -l job-name=s3hello)
+
+# Step 4: Get the credentials
+printf "____________________________________________________________________________________\n"
+printf "## Temporary credentials...\n"
+kubectl logs $pod_name -c aws-wlinitcontainer -n internal2cdemo --tail 100 -f > credentials.log &
+
+# Step 5: Wait for credentials to be fetched
+while ! grep -q "aws_access_key_id" credentials.log; do
+  sleep 5
+done
+
+# Step 6: Get the logs from the s3hello pod
+printf "____________________________________________________________________________________\n"
+printf "## Output from the workload logs...\n"
+kubectl logs $pod_name -c s3hello -n internal2cdemo --tail 100 -f
+
+# Step 6: Clean up
+rm credentials.log
+kubectl logs $pod_name -c aws-wlinitcontainer -n internal2cdemo > /dev/null 2>&1
